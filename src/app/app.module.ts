90d3a9a5d6474b86076ba356e95e78b76814ad43
sly96 <sLy@contractor.mcwe.com>
2020-12-26 19:20:12 -0800

    added about me component base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SplashComponent } from './pages/splash/splash.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SplashComponent
+    SplashComponent,
+    AboutMeComponent
   ],
   imports: [
     SharedModule,
