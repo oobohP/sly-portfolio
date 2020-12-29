3d9106f12cd868d6cffce31f3300f23c931a9c28
sly96 <sLy@contractor.mcwe.com>
2020-12-29 02:40:41 -0800

    added controller logic for resume

--- src/app/pages/splash/splash.component.ts
+++ src/app/pages/splash/splash.component.ts
@@ -1,4 +1,8 @@
+import { DialogComponent } from './../../shared/dialog/dialog.component';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-splash',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashComponent implements OnInit {
 
-  constructor() { }
+  resumeStatus = true;
+
+  constructor(
+    public dialog: MatDialog,
+    private _snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
   }
 
+  // Show's resume on correct password, otherwise shows user error
+  showResume(): void {
+    this.dialog.open(DialogComponent, {
+      disableClose: false,
+    }).afterClosed().subscribe(response => {
+      if (response) {
+        if (response.resumePassword === '') {
+          window.open('/assets/resume.pdf');
+        } else {
+          this._snackBar.open('Wrong Password Entered!', 'Done', {
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+            duration: 3000
+          });
+        }
+      }
+    });
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,16 +4,21 @@ import { MaterialModule } from './material/material.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CompaniesComponent } from './companies/companies.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
     CompaniesComponent,
-    CarouselComponent
+    CarouselComponent,
+    DialogComponent
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
     MaterialModule,
   ],
   exports: [
